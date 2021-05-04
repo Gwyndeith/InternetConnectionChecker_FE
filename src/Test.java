03817894fa2ef7b9724fb4346c7bc43aeb448b14
Orkun Doğan <orkun.dogan@ozu.edu.tr>
2021-05-04 05:45:34 +0300

    ping test is implemented

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,9 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -18,53 +20,75 @@ public class Test {
 //            socket = new Socket("localhost", 9000);
             oos = new ObjectOutputStream(socket.getOutputStream());
             ois = new ObjectInputStream(socket.getInputStream());
-            System.out.println((String) ois.readObject());
+            System.out.println("Server says: " + ois.readUTF());
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
+        Scanner scanner = new Scanner(System.in);
+        label:
         while(true) {
-            Scanner scanner = new Scanner(System.in);
+            System.out.print("ping, download or upload: ");
             String inputLine = scanner.nextLine();
-            if (inputLine.equals("exit"))
-                break;
-            else if (inputLine.equals("ping"))
-                runPingTest(oos, ois);
-            else if (inputLine.equals("download"))
-                runDownloadTest(oos, ois);
-            else if (inputLine.equals("upload"))
-                runUploadTest(oos, ois);
+            switch (inputLine) {
+                case "exit":
+                    try {
+                        //Sends the required message to the BE server so that it can close the connection.
+                        assert oos != null;
+                        oos.writeUTF("closeConnection");
+                        oos.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break label;
+                case "ping":
+                    assert oos != null;
+                    assert ois != null;
+                    System.out.println("Your ping is: " + runPingTest(oos, ois) + " ms");
+                    break;
+                case "download":
+                    assert oos != null;
+                    runDownloadTest(oos, ois);
+                    break;
+                case "upload":
+                    assert oos != null;
+                    runUploadTest(oos, ois);
+                    break;
+            }
         }
     }
 
-    public static void runPingTest(ObjectOutputStream oos, ObjectInputStream ois) {
+    public static long runPingTest(ObjectOutputStream oos, ObjectInputStream ois) {
+        long startTime = 0;
+        long endTime = 0;
         try {
-            starttime=oos.writeObject(System.currentTimeMillis());
-            URL url = new URL ("https://uploadpath");
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
-            private void sendFile(OutputStream out, String name, InputStream in, String fileName) {
-                String o = "Content-Disposition: form-data; name=\"" + URLEncoder.encode(name,"UTF-8")
-                        + "\"; filename=\"" + URLEncoder.encode(filename,"UTF-8") + "\"\r\n\r\n";
-                out.write(o.getBytes(StandardCharsets.UTF_8));
-                byte[] buffer = new byte[2048];
-                for (int n = 0; n >= 0; n = in.read(buffer))
-                    out.write(buffer, 0, n);
-                out.write("\r\n".getBytes(StandardCharsets.UTF_8));
+            startTime = System.currentTimeMillis();
+            //Send pingTest message to the server
+            oos.writeUTF("pingTest");
+            oos.flush();
+            //Give the start time to server? No need for ping test.
+            //oos.writeObject(startTime);
+
+            //Do not need to send any file to server for ping test
+            //sendFile(oos, "pingTest", ois, "");
+            String receivedMessage = ois.readUTF();
+            System.out.println("Server says: " + receivedMessage);
+            if ("message received".equals(receivedMessage))
+                endTime = System.currentTimeMillis();
+            else {
+                System.out.println("Something went wrong with connection to server.");
+                return -1;
             }
-            endtime=oos.writeObject(System.currentTimeMillis());
-            return endtime-starttime;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return (int)(endTime - startTime);
     }
 
     public static void runDownloadTest(ObjectOutputStream oos, ObjectInputStream ois) {
         try {
-            starttime =oos.writeObject(System.currentTimeMillis());
+            long startTime = System.currentTimeMillis();
+            oos.writeUTF(String.valueOf(startTime));
             File fileName = new File ("testfile.txt");
             URL url = new URL("http://DownloadFilePath");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -79,7 +103,7 @@ public class Test {
             }
             in.close();
             con.disconnect();
-            endtime=oos.writeObject(System.currentTimeMillis());
+            long endTime = System.currentTimeMillis();
             // Determine size of file
             //return size/(endtime-starttime);
         } catch (IOException e) {
@@ -89,25 +113,24 @@ public class Test {
 
     public static void runUploadTest(ObjectOutputStream oos, ObjectInputStream ois) {
         try {
-            starttime=oos.writeObject(System.currentTimeMillis());
-            URL url = new URL ("https://uploadpath");
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
-            private void sendFile(OutputStream out, String name, InputStream in, String fileName) {
-                String o = "Content-Disposition: form-data; name=\"" + URLEncoder.encode(name,"UTF-8")
-                        + "\"; filename=\"" + URLEncoder.encode(filename,"UTF-8") + "\"\r\n\r\n";
-                out.write(o.getBytes(StandardCharsets.UTF_8));
-                byte[] buffer = new byte[2048];
-                for (int n = 0; n >= 0; n = in.read(buffer))
-                    out.write(buffer, 0, n);
-                out.write("\r\n".getBytes(StandardCharsets.UTF_8));
-            }
-            endtime=oos.writeObject(System.currentTimeMillis());
+            long startTime = System.currentTimeMillis();
+            oos.writeUTF(String.valueOf(startTime));
+            oos.flush();
+
+            long endTime = System.currentTimeMillis();
             // Determine size of file
             //return size/(endtime-starttime);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void sendFile(OutputStream out, String name, InputStream in, String fileName) throws IOException {
+        String o = "Content-Disposition: form-data; name=\"" + URLEncoder.encode(name,"UTF-8") + "\"; filename=\"" + URLEncoder.encode(fileName,"UTF-8") + "\"\r\n\r\n";
+        out.write(o.getBytes(StandardCharsets.UTF_8));
+        byte[] buffer = new byte[2048];
+        for (int n = 0; n >= 0; n = in.read(buffer))
+            out.write(buffer, 0, n);
+        out.write("\r\n".getBytes(StandardCharsets.UTF_8));
     }
 }
